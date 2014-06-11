c7eb20975c85afea31d063e1d82a913a04ada4e2
DMatev <deyanmatev@gmail.com>
2014-06-11 16:17:39 +0300

    new db
    monogolab db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://nodejitsu:fc97d1470e2222ce2dc420def6034b1d@troup.mongohq.com:10024/nodejitsudb750185251'
+	'url' : ' mongodb://lolbuddiesdbuser:lolbuddiesdbpassword@ds037698.mongolab.com:37698/lolbuddies'
 
 };
